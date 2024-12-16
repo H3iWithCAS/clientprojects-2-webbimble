8a84093adf0e1bac1a4763f327a841849b39852e
H3i <husnimubarok62127@gmail.com>
2024-12-16 15:38:43 +0700

    fix

--- bimble/views.py
+++ bimble/views.py
@@ -13,7 +13,7 @@ def login_view(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('children_list')
     return render(request, 'login.html')
 
 def blog(request):
